4bb2adc8e72d5eb365a0f4c56fe46bb573cc5db1
Kaleb <Kalebalebachew4@gmail.com>
2024-09-30 11:29:19 +0300

    chore: ditch vercel

--- src/api/index.ts
+++
@@ -1,7 +0,0 @@
-const { VercelRequest, VercelResponse } = require('@vercel/node');
-import app from '../app'
-
-export default (req: typeof VercelRequest, res: typeof VercelResponse) =>{
-
-    return app(req, res)
-}
