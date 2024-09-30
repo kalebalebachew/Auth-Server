38540e84c4ff0809551c9f1401d14f7c41c8cc79
Kaleb <Kalebalebachew4@gmail.com>
2024-09-30 10:29:12 +0300

    chore: public directory

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express ,{Request, Response} from 'express'
+import path from 'path';
 
 const app = express()
 const port = 3000 
 
+app.use(express.static(path.join(__dirname, '../public'))); 
 app.get('/',(req: Request,res:Response)=>{
     res.send('My auth server lol')
 
