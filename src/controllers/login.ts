45a1eec0238f29283e393e6de8310e4550a3f977
Kaleb <Kalebalebachew4@gmail.com>
2024-10-01 09:51:41 +0300

    feat: added a token generation

--- src/controllers/login.ts
+++ src/controllers/login.ts
@@ -1,38 +1,50 @@
 import { db } from "../config/db";
+import jwt from "jsonwebtoken";
 import { eq, lt, gte, ne } from "drizzle-orm";
 import { UsersTable } from "../database/schema";
 import { NextFunction, Request, Response } from "express";
 import bcrypt from "bcrypt";
 
-export const loginUser = async (req: Request, res: Response, next: NextFunction) => {
-    const { username, password } = req.body;
+export const loginUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { username, password } = req.body;
 
   try {
     const [user] = await db
       .select({
+        id: UsersTable.id,
         username: UsersTable.username,
         password: UsersTable.password,
+        role: UsersTable.role,
       })
       .from(UsersTable)
       .where(eq(UsersTable.username, username));
 
     if (!user) {
-      res
-        .status(404)
-        .json({ msg: "whoopsie no user with that username bro" });
+      res.status(404).json({ msg: "whoopsie no user with that username bro" });
     }
 
     const isMatch = await bcrypt.compare(password, user.password);
 
     if (!isMatch) {
-       res
-        .status(400)
-        .json({ msg: "wrong password you ain't getting in" });
+      res.status(400).json({ msg: "wrong password you ain't getting in" });
     }
 
-     res.status(200).json({ msg: "Login successful, there you go!" });
+    const token = jwt.sign(
+      {
+        id: user.id,
+        role: user.role,
+      },
+      process.env.JWT_SECRET as jwt.Secret,
+      { expiresIn: "1h" }
+    );
+
+    res.status(200).json({ token });
   } catch (error) {
     console.error(error);
-     res.status(500).json({ msg: "whoops the server tweaked" });
+    res.status(500).json({ msg: "whoops the server tweaked" });
   }
 };
